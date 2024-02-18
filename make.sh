9959ee30fc6447f67a4e9ed36707cb7890fc031b
Nuno M Reis <nmreis@mail.ru>
2024-02-18 23:50:07 +0000

    make lib on lua5.3

--- make.sh
+++ make.sh
@@ -1,19 +1,5 @@
 #! /bin/bash
-
 set -e
 
-echo "----> Going pedantic all over the source"
-
-# Ugh. hiredis.h is not c89-compatible
-echo "--> c89..."
-gcc -O2 -fPIC -I/usr/include -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/ -Wall --pedantic --std=c89 #-Werror
-
-echo "--> c99..."
-gcc -O2 -fPIC -I/usr/include -c src/lua-hiredis.c -o /dev/null -Isrc/ -Ilib/ -Wall --pedantic -Werror --std=c99
-
-# Ugh. hiredis.h is not c++98-compatible
-echo "--> c++98..."
-gcc -xc++ -O2 -fPIC -I/usr/include -c src/lua-hiredis.c -o /dev/null -Isrc/c/ -Ilib/ --pedantic -Wall --std=c++98 #-Werror
-
-echo "----> Making rock"
-luarocks --local make rockspec/lua-hiredis-scm-1.rockspec
+gcc -O2 -fPIC -I/usr/include/lua5.3 -c src/lua-hiredis.c -Isrc/ -Ilib/ --pedantic -Wno-implicit-function-declaration --std=c99
+gcc -shared -o hiredis.so lua-hiredis.o  -llua5.3 -lhiredis
