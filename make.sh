f61c0a38cb1de08560683d86c29e4b4671acd6c2
Nuno M Reis <nmreis@mail.ru>
2024-02-19 00:39:38 +0000

    no need to add -lhiredis

--- make.sh
+++ make.sh
@@ -2,4 +2,4 @@
 set -e
 
 gcc -O2 -fPIC -I/usr/include/lua5.3 -c src/lua-hiredis.c -Isrc/ -Ilib/ --pedantic -Wno-implicit-function-declaration --std=c99
-gcc -shared -o hiredis.so lua-hiredis.o  -llua5.3 -lhiredis
+gcc -shared -o hiredis.so lua-hiredis.o -llua5.3
